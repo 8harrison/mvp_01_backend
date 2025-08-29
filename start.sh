cb997961a834d9fc68a52fed828a4ecb5594f962
8harrison <monteiro4100@gmail.com>
2025-08-28 23:35:09 -0300

    Adicionando descrição no script de inicialização

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 source env/bin/activate
 flask run --host 0.0.0.0 --port 5000 --reload
+
+# Esse script serve para:
+# 1 - Ativar e entrar no bash do ambiente virtual
+# 2 - Inicializar o servidor
+# Para executar o start.sh basta abrir o terminal e executar
+# source start.sh
